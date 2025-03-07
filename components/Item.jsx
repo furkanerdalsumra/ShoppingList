705b7ec8245df73dded291584f4fef72a6cba397
Furkan Erdal SUMRA <furkanerdalsumra@hotmail.com>
2025-03-07 09:45:01 +0300

    Delete components directory

--- components/Item.jsx
+++
@@ -1,8 +0,0 @@
-export default function Item({ item, onDeleteItem }) {
-    return (
-      <li>
-        <span> {item.quantity} {item.title}</span>
-        <button onClick={() => onDeleteItem(item.id)}>X</button>
-      </li>
-    );
-  }
