0e807acd5665b2c6a2cfe2c47c3e08db987e86a1
furkanerdalsumra <furkanerdalsumra@hotmail.com>
2025-03-13 11:52:45 +0300

    Dev first commit

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
   const [items, setItems] = useState([]);
 
   function handleAddItem(item) {
-    setItems((items => [...items, item]));
+    setItems((items => [...items, item]))
   }
 
   function handleDeleteItem(id) {
